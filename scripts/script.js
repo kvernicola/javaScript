b7506eba815937bfa6ef200aa1619788b220d055
amgiribaldi <amgiribaldi@gmail.com>
2022-10-31 01:03:24 -0300

    Aplique solucion CoderAsk

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,60 @@
 const savedPassw = "qwerty";
 let accessGranted = false;
-
 let userName = prompt("Hola, ingrese su nombre");
 alert("Bienvenido/a, " + userName);
+let flag = true;
 
-let optSelected = prompt("\nPara poder jugar tienes que estar registrado. \nPresiona 1 para registrarte \nPresiona 2 para iniciar sesion \nPresiona x para salir.");
-while (optSelected != "x") {
+let optSelected = prompt(
+	"\nPara poder jugar tienes que estar registrado. \nPresiona 1 para registrarte \nPresiona 2 para iniciar sesion \nPresiona x para salir."
+);
+
+do {
 	switch (optSelected) {
 		case "1":
-			alert("El registro para nuevos usuarios se encuentra deshabilitado momentaneamente")
+			alert(
+				"El registro para nuevos usuarios se encuentra deshabilitado momentaneamente"
+			);
+			optSelected = prompt(
+				"\nPara poder jugar tienes que estar registrado. \nPresiona 1 para registrarte \nPresiona 2 para iniciar sesion \nPresiona x para salir."
+			);
 			break;
 		case "2":
-			login ();
+			login();
+			flag = false;
 			break;
-			// COMO SALGO DEL PROMP DEL WHILE CUANDO LA FUNCION LOGIN FINALIZA EN TRUE? 
+		case "x":
+			flag = false;
+			break;
 		default:
 			alert("La opcion elegida no es valida");
+			optSelected = prompt(
+				"\nPara poder jugar tienes que estar registrado. \nPresiona 1 para registrarte \nPresiona 2 para iniciar sesion \nPresiona 'x' para salir."
+			);
 			break;
 	}
-	optSelected = prompt("\nPara poder jugar tienes que estar registrado. \nPresiona 1 para registrarte \nPresiona 2 para iniciar sesion \nPresiona 'x' para salir.");
-}
 
+}while (flag);
 
-function login () {
-	//let accessGranted = false;
-
+function login() {
 	for (let i = 2; i >= 0; i--) {
 		let userPassw = prompt(
-			"Ingrese tu password " + userName+"." +" Tienes " + (i + 1) + " intentos."
+			"Ingrese tu password " +
+				userName +
+				"." +
+				" Tienes " +
+				(i + 1) +
+				" intentos."
 		);
 		if (userPassw == savedPassw) {
 			accessGranted = true;
-			alert("Acceso correcto" )
+			alert("Acceso correcto");
 			break;
-		}else {
-			alert("El password ingresado es incorrecto. Tienes " + i +" intentos.");
+		} else {
+			alert("El password ingresado es incorrecto. Tienes " + i + " intentos.");
 		}
 	}
 	return accessGranted;
 }
 
-
-
-
+//-----------------------------------------------
 
